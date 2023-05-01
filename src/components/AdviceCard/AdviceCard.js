630e1324d68ff2eca89cba1eff85e18728492389
jacky-ui <jacky.du.004@gmail.com>
2023-04-30 21:31:30 -0400

    create basic skeleton for card

--- src/components/AdviceCard/AdviceCard.js
+++ src/components/AdviceCard/AdviceCard.js
@@ -1,9 +1,17 @@
 import './AdviceCard.scss';
+import desktopDivider from '../../assets/images/pattern-divider-desktop.svg';
+import mobileDivider from '../../assets/images/pattern-divider-mobile.svg';
 
 function AdviceCard() {
     return(
-        <>
-        </>
+        <section className='card'>
+            <h2 className='card__header'>Advice #117</h2>
+            <p className='card__text'></p>
+            <img 
+                src={desktopDivider}
+                alt=''
+            />
+        </section>
     )
 };
 
