fc9b7ef08026e07a710aae8b68eecba0ee17e105
jacky-ui <jacky.du.004@gmail.com>
2023-05-01 22:39:41 -0400

    able to pass api data as props and dynamic render on card

--- src/components/AdviceCard/AdviceCard.js
+++ src/components/AdviceCard/AdviceCard.js
@@ -3,11 +3,12 @@ import desktopDivider from '../../assets/images/pattern-divider-desktop.svg';
 import mobileDivider from '../../assets/images/pattern-divider-mobile.svg';
 
 function AdviceCard(props) {
-    const { id, advice } = props.adviceApi
+    console.log(props.adviceObject)
+    const { id, advice } = props.adviceObject
     return(
         <section className='card'>
-            <h2 className='card__header'>Advice #117</h2>
-            <p className='card__text'>some text here with qoutes</p>
+            <h2 className='card__header'>{`Advice #${id}`}</h2>
+            <p className='card__text'>{advice}</p>
             <img 
                 src={desktopDivider}
                 alt=''
